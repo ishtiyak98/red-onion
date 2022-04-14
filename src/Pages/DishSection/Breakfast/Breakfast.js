e388fbf8cb29e5b6e0d39a67df696ac335d4dd9c
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-15 01:20:02 +0600

    fetch json for b-data

--- src/Pages/DishSection/Breakfast/Breakfast.js
+++ src/Pages/DishSection/Breakfast/Breakfast.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Breakfast = () => {
+    const [breakFast, setBreakFast]=useState([]);
+
+    useEffect( ()=>{
+        fetch("breakfast.json")
+        .then(res=> res.json())
+        .then(data => setBreakFast(data));
+    } ,[]);
+
+    console.log(breakFast);
+
     return (
         <div>
            Braekfas 
--- src/Pages/DishSection/DishSection.js
+++ src/Pages/DishSection/DishSection.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Breakfast from "./Breakfast/Breakfast";
-import DishNav from "./DishNav/DishNav";
 
 const DishSection = () => {
+    const [displayFood, setDisplayFood] = useState('Breafast');
+
+    const handleBreakfast = ()=>{
+        setDisplayFood("breakfast");
+    }
+
+    const handleLunch = ()=> {
+        setDisplayFood("lunch");
+    } 
+    
+    const handleDinner = ()=> {
+        setDisplayFood("dinner");
+    }
+
+    if(displayFood==="lunch"){
+        
+        console.log(displayFood);
+    }
+
   return (
     <div className="my-28">
-      <DishNav></DishNav>
-      <Breakfast></Breakfast>
+      <nav className="flex justify-center space-x-24">
+        <button onClick={handleBreakfast} className="text-2xl font-semibold text-slate-800 pb-1 lg:px-3 border-b-2 border-w hover:text-red-500 hover:border-b-red-500 hover:cursor-pointer">
+          Breakfast
+        </button>
+        <button onClick={handleLunch} className="text-2xl font-semibold text-slate-800 pb-1 lg:px-3 border-b-2 border-w hover:text-red-500 hover:border-b-red-500 hover:cursor-pointer">
+          Lunch
+        </button>
+        <button onClick={handleDinner} className="text-2xl font-semibold text-slate-800 pb-1 lg:px-3 border-b-2 border-w hover:text-red-500 hover:border-b-red-500 hover:cursor-pointer">
+          Dinner
+        </button>
+      </nav>
+
+      {
+          displayFood==="lunch" ? <Breakfast></Breakfast> : <Breakfast></Breakfast>
+      }
     </div>
   );
 };
